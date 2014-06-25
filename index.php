f72e74ad1d0c423c9c9f53cd9b1a2aff17aafd9b
angelapap1990 <angelamelodica@gmail.com>
2014-06-25 22:41:43 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Φροντιστήριο - Αγγλικά &amp; Πληροφορική - Κατερίνα Γκρίνια &amp; Αγγελική Παπαστεργίου - Αλμυρός 37100, Ιάσονος 14">
+		<meta name="author" content="">
+		<title>Αγγλικά & Πληροφορική ΖΕΥΞΙΣ | Κατερίνα Γκρίνια & Αγγελική Παπαστεργίου | Αλμυρός, Ιάσονος 14</title>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,greek-ext' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">	
+		<link rel="icon" type="image/png" href="img/zefksis.png" />	
+	</head>
+
+	<body>
+		<div id="top-section"><h1 class="ir logo">Φροντιστήριο Ζεύξις | Αγγλικά & Πληροφορκή | Κατερίνα Γκρίνια & Αγγελική Παπαστεργίου | Αλμυρός</h1></div>
+
+		<div id="bottom-section">
+			<h2>Σύντομα κοντά σας</h2>
+			<div class="container">
+				<p>Από 1η Αυγούστου θα λειτουργήσει η ιστοσελίδα του νέου μας φροντιστηρίου, η οποία, εκτός από τον ενημερωτικό της χαρακτήρα θα αποτελεί και μια σύγχρονη πλατφόρμα ηλεκτρονικής εκπαίδευσης, χρήσιμη τόσο στο μαθητή όσο και στο γονιό.</p>
+			</div>
+			
+
+			<!-- ~ Countdown ~ -->
+			<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+			<div id="counter-outer">
+				<div id="countdown">
+					<!-- ~ Days left ~ -->
+					<div class="time-indication">
+						<div id="days-left">ημέρες<br><span class="days"></span></div>
+					</div>
+
+					<!-- ~ Hours left ~ -->
+					<div class="time-indication">
+						<div id="days-left">ώρες<br><span class="hours"></span></div>
+					</div>
+
+					<!-- ~ Mins left ~ -->
+					<div class="time-indication">
+						<div id="days-left">λεπτά<br><span class="minutes"></span></div>
+					</div>
+
+					<!-- ~ Secs left ~ -->
+					<div class="time-indication">
+						<div id="days-left">δεύτερα<br><span class="seconds"></span></div>
+					</div>												
+				</div>
+			</div>
+		</div>
+
+		<!-- ~ Contact us ~ -->
+		<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+		<div id="contact">
+			<div class="container">
+				<h2>Επικοινωνία</h2>
+				<p>
+					<span class="glyphicon glyphicon-map-marker"></span> Ιάσονος 14, Αλμυρός 37100 | 
+					<span class="glyphicon glyphicon-phone-alt"></span> 24220 22498 | 
+					<span class="glyphicon glyphicon-phone"></span> 6976910873 | 
+					<span class="glyphicon glyphicon-envelope"></span> info@zefksis.com 
+				</p>
+			</div>
+		</div>
+		<div id="map_canvas" class="map"></div>
+
+		<!-- ~ Footer ~ -->
+		<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+		<div class="footer">Copyright &copy; 2014 | ΖΕΥΞΙΣ</div>
+
+		<!-- SCRIPT -->
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/respond.min.js"></script>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=FALSE"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
